23179dd5e5309c9dd46f22037517bbee9e5170ae
Nikolas Kovacs <kovacsn00@gmail.com>
2022-06-18 02:35:16 -0400

    Add getters

--- sprite.py
+++ sprite.py
@@ -1,10 +1,10 @@
 class Sprite:
     def __init__(self, sprite_index, image, x, y, width, height, rotation=0, description='') -> None:
         self.image = image
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.rotation = rotation
-        self.index = sprite_index
+        self.x = int(x)
+        self.y = int(y)
+        self.width = int(width)
+        self.height = int(height)
+        self.rotation = int(rotation)
+        self.index = int(sprite_index)
         self.desc = description
